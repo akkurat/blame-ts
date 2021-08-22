30c4d257148551eb7658b064a1ea18eebc2227a0
Moritz Vifian <moritz.vifian@gmx.ch>
2021-08-22 15:47:34 +0200

    Rewriting tests to Typscript
    * ignoring additional fields
    * adding test trailing whitespace

--- src/blame-diff.spec.ts
+++ src/blame-diff.spec.ts
@@ -1,13 +1,6 @@
 import { expect } from "chai";
 import { blame, StringExtractor } from "./blame";
 
-describe('calculate', function () {
-    it('add', function () {
-        const result = 2 + 5;
-        expect(result).equal(7);
-    });
-});
-
 describe('blame-only', function () {
     it('add-only forw', function () {
         const result = blame(snapshots, new StringExtractor())
--- src/original-blamejs.spec.ts
+++ src/original-blamejs.spec.ts
@@ -1,14 +1,31 @@
 import { describe, it } from 'mocha'
 import { expect } from 'chai'
-import blamejs from './index'
+import { blame } from './index'
+import { IExtractor, StringExtractor } from './blame'
+import * as chai from 'chai';
+import chaiExclude from 'chai-exclude';
 
-describe('blamejs', () => {
+chai.use(chaiExclude);
+
+type snapshot = {
+  code: string
+  id: string
+}
+
+const extractor: IExtractor<snapshot, string> = {
+  getCode: (item: snapshot): string => item.code,
+  getOrigin: (item: snapshot): string => item.id,
+}
+
+const excludeAddinfo = ['diff', 'diffentry', 'lineindiff']
+
+describe('blame', () => {
   it('is a function', () => {
-    expect(blamejs).to.be.a('function')
+    expect(blame).to.be.a('function')
   })
 
   it('returns array containing every line and its origin', () => {
-    const codes: Array = [
+    const codes: Array<snapshot> = [
       {
         id: '0',
         code: `
@@ -37,12 +54,10 @@ describe('blamejs', () => {
         `,
       },
     ]
-    const options: Object = {
-      getCode: (item: Object): string => item.code,
-      getOrigin: (item: Object): string => item.id,
-    }
 
-    expect(blamejs(codes, options)).to.be.a('Array').and.deep.equal([
+    expect(blame(codes, extractor)).to.be.a('Array')
+    .excludingEvery(excludeAddinfo)
+    .and.deep.equal([
       {
         origin: '2',
         value: '',
@@ -67,7 +82,7 @@ describe('blamejs', () => {
   })
 
   it('returns array containing every line and its index', () => {
-    const codes: Array = [
+    const codes: Array<string> = [
       `
         REPORT test.
 
@@ -89,7 +104,9 @@ describe('blamejs', () => {
       `,
     ]
 
-    expect(blamejs(codes)).to.be.a('Array').and.deep.equal([
+    expect(blame(codes, new StringExtractor())).to.be.a('Array')
+    .excludingEvery(excludeAddinfo)
+    .and.deep.equal([
       {
         origin: 2,
         value: '',
@@ -118,7 +135,7 @@ describe('blamejs', () => {
   })
 
   it('returns array containing every line and its origin with trimmed source code', () => {
-    const codes: Array = [
+    const codes: Array<snapshot> = [
       {
         id: '0',
         code: `
@@ -148,12 +165,10 @@ describe('blamejs', () => {
         `,
       },
     ]
-    const options: Object = {
-      getCode: (item: Object): string => item.code,
-      getOrigin: (item: Object): string => item.id,
-    }
 
-    expect(blamejs(codes, options)).to.be.a('Array').and.deep.equal([
+    expect(blame(codes, extractor)).to.be.a('Array')
+    .excludingEvery(excludeAddinfo)
+    .and.deep.equal([
       {
         origin: '2',
         value: '',
@@ -161,6 +176,68 @@ describe('blamejs', () => {
       {
         origin: '1',
         value: '          REPORT test2.',
+      },
+      {
+        origin: '2',
+        value: '',
+      },
+      {
+        origin: '0',
+        value: "          WRITE 'a'.",
+      },
+      {
+        origin: '1',
+        value: "          WRITE 'd'.",
+      },
+      {
+        origin: '2',
+        value: '',
+      },
+    ])
+  })
+
+  it('returns array containing every line and its origin with whitespace eol', () => {
+    const codes: Array<snapshot> = [
+      {
+        id: '0',
+        code: `
+          REPORT test2. 
+
+          WRITE 'a'.
+          WRITE 'd'.
+        `,
+      },
+      {
+        id: '1',
+        code: `
+          REPORT test2. 
+
+          WRITE 'b'.
+          WRITE 'c'.
+          WRITE 'd'.
+        `,
+      },
+      {
+        id: '2',
+        code: `
+          REPORT test. 
+
+          WRITE 'a'.
+          WRITE 'c'.
+        `,
+      },
+    ]
+
+    expect(blame(codes, extractor)).to.be.a('Array')
+    .excludingEvery(excludeAddinfo)
+    .and.deep.equal([
+      {
+        origin: '2',
+        value: '',
+      },
+      {
+        origin: '1',
+        value: '          REPORT test2. ',
       },
       {
         origin: '2',
